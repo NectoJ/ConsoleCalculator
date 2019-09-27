0b2896ee03928fef2acf315f32020ac9a93346c7
Jamie Lam <jamie.lam@lernende.bbw.ch>
2019-09-27 14:38:55 +0200

    Added a class which can create a sum from two numbers

---
+++ ConsoleCalculator/src/main/java/ch/bbw/ConsoleCalculator/Calculator.java
@@ -0,0 +1,18 @@
+package ch.bbw.ConsoleCalculator;
+
+/**
+ * Calculator
+ * - Creates a sum from two numbers together
+ * 
+ * 
+ * @author Jamie Lam
+ * @version 0.0.1
+ */
+
+public class Calculator {
+	
+	public int sum(int sum1, int sum2) {
+		return sum1 + sum2;
+	}
+	
+}
