cfdced4c6197c4dc5fc94ac98ad2975d6f0a860d
Jamie Lam <jamie.lam@lernende.bbw.ch>
2019-12-06 14:10:36 +0100

    Added JUnit test for method "sum()"

--- ConsoleCalculator/src/main/java/ch/bbw/ConsoleCalculator/App.java
+++ ConsoleCalculator/src/main/java/ch/bbw/ConsoleCalculator/App.java
@@ -11,13 +11,11 @@ public class App
     public static void main( String[] args )
     {
     	Calculator calculator = new Calculator();
-    	int firstValue = 0;
-    	int secondValue = 0;
     	
     	System.out.print("Console Calculator" + "\n\n" );
     	
-    	firstValue = 10;
-		secondValue = 20;
+    	int firstValue = 10;
+		int secondValue = 20;
         System.out.println( "First Sum: " + firstValue + " + " + secondValue + " = " + calculator.sum(10, 20) );
     }
 }
